7c688ffd5036b153f47ae6d69763e645c0b4aa3f
Jan Jankowski <jjankowski@novomatic-tech.com>
2018-08-22 19:47:17 +0200

    change folder structure

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,7 +3,7 @@ import Logo from '../../Logo/Logo';
 import NavigationItems from './../NavigationItems/NavigationItems'
 import classes from './SideDrawer.css';
 import BackDrop from '../../UI/Backdrop/Backdrop';
-import Auxiliary from '../../../hoc/Auxiliary';
+import Auxiliary from '../../../hoc/Auxiliary/Auxiliary';
 
 const sideDrawer =(props) => {
         let attachedClasses = [classes.SideDrawer, classes.Close];
@@ -15,7 +15,7 @@ const sideDrawer =(props) => {
              <BackDrop show={props.open} clicked={props.closed}/>
             <div className={attachedClasses.join(' ')}>
                 <div className={classes.Logo}>
-                    <Logo onClick={props.closed}  />
+                    <Logo   />
                 </div>
                 <nav>
                     <NavigationItems />
