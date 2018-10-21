16857299fb4a2a41267898cbe427d228dbed5d59
Jan Jankowski <jan.jankowski@sabre.com>
2018-10-21 11:13:26 +0200

    add handling input form

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -8,13 +8,68 @@ import Input from '../../../components/UI/Input/Input'
 
 class ContactData extends Component {
     state = {
-        name: '',
-        email: '',
-        address: {
-            street: '',
-            postalCode: ''
+        orderForm: {
+            name: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your Name'
+                },
+                value: ''
+            },
+            street: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Street'
+                },
+                value: ''
+            },
+            zipCode: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'ZIP CODE'
+                },
+                value: ''
+            },
+            country: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Country'
+                },
+                value: ''
+            },
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Your e-mail'
+                },
+                value: ''
+            },
+            deliveryMethod: {
+                elementType: 'select',
+                elementConfig: {
+                    options: [{value: 'fastest', displayValue: 'Fastest'},
+                        {value: 'cheapest', displayValue: 'Cheapest'}],
+                },
+                value: ''
+            },
         },
         loading: false
+    }
+    inputChangedHandler = (event, inputIdentifier) => {
+        const updatedOrderForm = {
+            ...this.state.orderForm
+        }
+        const updatedFormElement = {
+            ...updatedOrderForm[inputIdentifier]
+        };
+        updatedFormElement.value = event.target.value;
+        updatedOrderForm[inputIdentifier] = updatedFormElement;
+        this.setState({orderForm:updatedOrderForm});
     }
     orderHandler = (event) => {
         event.preventDefault();
@@ -22,16 +77,7 @@ class ContactData extends Component {
         const order = {
             ingredients: this.props.ingredients,
             price: this.props.price,
-            customer: {
-                name: this.state.name,
-                address: {
-                    street: this.state.address.street,
-                    zipCode: this.state.address.postalCode,
-                    country: 'Poland'
-                },
-                email: this.state.email,
-            },
-            deliveryMethod: 'fastest'
+
         }
         axios.post('/orders.json', order)
             .then(response => {
@@ -43,12 +89,23 @@ class ContactData extends Component {
             });
     }
 
-    render(){
-        let form =(<form>
-            <Input  inputtype="text" name="name" placeholder="Your name"/>
-            <Input  inputtype="text" name="email" placeholder="Your email"/>
-            <Input  inputtype="text" name="street" placeholder="Your street"/>
-            <Input  inputtype="text" name="postalCode" placeholder="Your Postal Code"/>
+    render() {
+        const formsElementsArray = [];
+        for (let key in this.state.orderForm) {
+            formsElementsArray.push({
+                id: key,
+                config: this.state.orderForm[key]
+            })
+        }
+        let form = (<form>
+            {formsElementsArray.map(formElement => (
+                <Input key={formElement.id}
+                    elementType={formElement.config.elementType}
+                    elementConfig={formElement.config.elementConfig}
+                    value={formElement.config.value}
+                    changed={(event) => this.inputChangedHandler(event,formElement.id)}
+                />
+            ))}
             <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
         </form>);
         if (this.state.loading) {
@@ -62,6 +119,6 @@ class ContactData extends Component {
         )
     }
 
-    }
+}
 
 export default ContactData;
